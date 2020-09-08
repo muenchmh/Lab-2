ff893fd8c0c3d4450f0c8337e9518b21c68de679
Mike Muenchen <michaelhmuenchen@gmail.com>
2020-09-08 16:38:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Counts");
+
+    int total;
+    total = 55 * 10;
+
+    double blue;
+    blue = total * .24;
+
+    double brown;
+    brown = total * .13;
+
+    double green;
+    green = total * .16;
+
+    double orange;
+    orange = total * .2;
+
+    double red;
+    red = total * .13;
+
+    double yellow;
+    yellow = total * .14;
+
+    System.out.println("blue: " + blue);
+    System.out.println("brown: " + brown);
+    System.out.println("green: " + green);
+    System.out.println("orange: " + orange);
+    System.out.println("red: " + red);
+    System.out.println("yellow: " + yellow);
+    }
+}
